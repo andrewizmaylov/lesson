56e8ae6a6dd708d3ae8b0d9078b1f4bdc8d55871
andrewizmaylov <andrew.izmaylov@gmail.com>
2023-11-05 21:00:05 +0200

    no message

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,7 +4,8 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down) || true
+#(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
     git fetch origin main
     git reset --hard origin/main
