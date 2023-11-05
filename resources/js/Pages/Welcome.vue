bb5434ccfacb659dc2559d56f2112b43b25a2731
andrewizmaylov <andrew.izmaylov@gmail.com>
2023-11-05 22:05:33 +0200

    pevdf

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -21,6 +21,7 @@ defineProps({
 
                 <Link v-if="canRegister" :href="route('register')" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
             </template>
+	        <h1>pre</h1>
         </div>
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
