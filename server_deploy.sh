1797916132ac30d5dc36e56c480ad1805f324dcc
Andrew Izmaylov <andrewizmaylov@users.noreply.github.com>
2023-11-05 21:01:54 +0200

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -5,7 +5,7 @@ echo "Deploying application ..."
 
 # Enter maintenance mode
 (php artisan down) || true
-#(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+
     # Update codebase
     git fetch origin main
     git reset --hard origin/main
